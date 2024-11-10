337f3289eff0a300ede14a7b640fc3d5461d919b
Yvette334 <y.muhoracye@alustudent.com>
2024-11-10 11:02:26 -0800

    pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        t = triangles[-1]
+        tmp = [1]
+        for i in range(len(t) - 1):
+            tmp.append(t[i] + t[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles

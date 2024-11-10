e491fa87e4ede7d39005f2167b07025de1b2ad63
Yvette334 <y.muhoracye@alustudent.com>
2024-11-10 06:23:02 -0800

    append write

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a file-appending function."""
+
+
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)

2d3afff5f65788964fcb4dea8a601b851cf46617
Yvette334 <y.muhoracye@alustudent.com>
2024-11-17 08:19:09 -0800

    added to repository

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 A Python script that takes in a URL, sends a request to the URL,
-and displays the body of the response.""" 
+and displays the body of the response"""
 import requests
 import sys
 if __name__ == "__main__":

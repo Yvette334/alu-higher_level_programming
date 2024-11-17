97b1ac7fbb8cf04f8446ca312c01426daaad7001
Yvette334 <y.muhoracye@alustudent.com>
2024-11-17 05:51:50 -0800

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    request = urllib.request.Request(url)
-    
-    try:
+    urls = [
+        "https://intranet.hbtn.io/status",
+        "http://0.0.0.0:5050/status"
+    ]
+
+    for url in urls:
+        request = urllib.request.Request(url)
         with urllib.request.urlopen(request) as response:
             body = response.read()
             print("Body response:")
-            print("\t- type: {}".format(type(body)))  # Expected: <class 'bytes'>
-            print("\t- content: {}".format(body))    # Expected: b'OK'
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))  # Expected: OK
-    except urllib.error.URLError as e:
-        print(f"Error: {e}")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))

5b71aaa31f87c311fa921522baecfb57ce0cadb4
Yvette334 <y.muhoracye@alustudent.com>
2024-11-23 07:26:22 -0800

    adding to repository

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+"""Fetches http://0.0.0.0:5050/status."""
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    # Use the updated URL
+    r = requests.get("http://0.0.0.0:5050/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))

8bdc7c6c54138f94443bba93e92bd15f0346d9d0
Yvette334 <y.muhoracye@alustudent.com>
2024-11-30 05:12:51 -0800

    add to repository

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""Fetches from both http://0.0.0.0:5050/status and https://intranet.hbtn.io/status."""
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
 import requests
 
-def fetch_url(url):
-    """Fetch a URL and print its body response."""
-    try:
-        # Fetch the URL
-        r = requests.get(url)
-        print(f"Fetching {url}")
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.text))
-    except requests.exceptions.RequestException as e:
-        # Handle exceptions and print the error
-        print(f"Error fetching {url}: {e}")
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    # Fetch from both URLs
-    fetch_url("http://0.0.0.0:5050/status")  # For the local web server
-    fetch_url("https://intranet.hbtn.io/status")  # For the external web server
-
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))

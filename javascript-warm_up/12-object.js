d8e765ee5d1985369937ce281228dfc98b199a48
Yvette334 <y.muhoracye@alustudent.com>
2025-01-21 12:40:12 -0800

    added to repository

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -6,3 +6,12 @@ const myobject = {
 console.log(myobject);
 myobject.value = 89;
 console.log(myobject);
+
+#!/usr/bin/node
+const myObject = {
+        type: 'object',
+        value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);

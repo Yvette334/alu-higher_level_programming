78dfe4d5f011cb59baabcc2cc3d2b1ba65c32b4a
Yvette334 <y.muhoracye@alustudent.com>
2025-01-21 00:37:21 -0800

    added to repository

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,4 +3,5 @@ const myNumber = Math.floor(Number(process.argv[2]));
 if (isNaN(myNumber)) {
   console.log('Not a number');
 } else {
-  console.log('My number:', myNumber
+  console.log('My number:', myNumber);
+}

b70e519ad819e7a2777d782c52618a0cca9c5c49
Yvette334 <y.muhoracye@alustudent.com>
2025-01-21 00:08:48 -0800

    added to repository

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const count = process.argv.length;
 if (count > 3) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 } else if (count === 3) {
-	console.log('Argument found');
-} else{
-	console.log('No argument');
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }

bbc8a1c0a3d45ce4f4e863a200af161f0db6030c
Yvette334 <y.muhoracye@alustudent.com>
2025-01-22 02:41:46 -0800

    added to repository

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -7,20 +7,20 @@ module.exports = class Rectangle {
     }
   }
 
-
   print () {
-    for (let i=0; i < this.height; i++) {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
-   }
-  }
-rotate () {
-  const temp = this.width;
-  this.width = this.height;
-  this.height = temp;
+    }
   }
 
-double () {
-  this.width *= 2;
-  this.height *= 2;
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 };

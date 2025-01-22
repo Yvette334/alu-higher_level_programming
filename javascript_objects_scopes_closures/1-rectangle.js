456d3878fc13beca88813078f5b8d2762f3566f1
Yvette334 <y.muhoracye@alustudent.com>
2025-01-22 01:57:02 -0800

    added to repository

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -3,5 +3,12 @@ module.exports = class Rectangle {
   constructor (w,h) {
     this.width = w;
     this.height = h;
-	}
+  }
+};
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
